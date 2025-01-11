ea9ecff8798eabe114b70877742eb6a3ffc37a59
BarelyBear <kangas.otso@gmail.com>
2025-01-11 15:30:17 +0200

    Intermediate commit. Fixed semantic error. Triangle on screen.

--- Getting_Started/HelloTriangle/main.cpp
+++ Getting_Started/HelloTriangle/main.cpp
@@ -10,6 +10,20 @@ void processInput(GLFWwindow *window);
 const unsigned int SCR_WIDTH = 666;
 const unsigned int SCR_HEIGHT = 666;
 
+const char *vertexShaderSource = "#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "}\0"; 
+
+const char *fragmentShaderSource = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+ "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+"}\0";
+
 int main()
 {
     // glfw: initialize and configure
@@ -41,8 +55,87 @@ int main()
     {
         std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
-    }    
+    }
 
+    /* Setting up shaders
+    Vertex shader: issue creation from OpenGL, send sourcecode, compile, check for errors. Same algo for fragment.
+    */ 
+
+    unsigned int vertexShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader,1,&vertexShaderSource,NULL);
+    glCompileShader(vertexShader);
+
+    int success;
+    char infoLog[512];
+    glGetShaderiv(vertexShader,GL_COMPILE_STATUS, &success);
+
+    if(!success) {
+        glGetShaderInfoLog(vertexShader,512,NULL,infoLog);
+        std::cout << "Vertex Shader not compiled\n" << infoLog << std::endl;
+    }
+
+    unsigned int fragmentShader;
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader,1, &fragmentShaderSource,NULL);
+    glCompileShader(fragmentShader);
+
+    glGetShaderiv(fragmentShader,GL_COMPILE_STATUS, &success);
+    if(!success) {
+        glGetShaderInfoLog(fragmentShader,512,NULL,infoLog);
+        std::cout << "Fragment Shader not compiled\n" << infoLog << std::endl;
+    }
+
+    /*
+    High level shader program : 
+    linked version of executable and shaders. 
+    This program is what we use in the main render loop.
+    */ 
+
+    unsigned int shaderProgram;
+    shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram,vertexShader);
+    glAttachShader(shaderProgram,fragmentShader);
+
+    /*
+    Just south of two hours of debugging I gave up and compared the source code to the tutorials.
+    Turns out I erased the linking command when I was trying to clean up the code to make it easier to debug and compare.
+    Live and learn!
+    */
+    glLinkProgram(shaderProgram);
+
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if(!success) {
+        glGetProgramInfoLog(shaderProgram,512,NULL,infoLog);
+    }
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+        float vertices[] = {
+           -0.5f, -0.5f, 0.0f,
+            0.5f, -0.5f, 0.0f,
+            0.0f,  0.5f, 0.0f
+    };
+
+        /*
+    Setting up data flow : 
+    1) Setup a vertex Array object
+    2) 
+    Setup Vertex Buffer Object and bind it
+    2) Stream (copy) the data from program to the bound buffer.
+    */
+
+    unsigned int VBO, VAO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1 , &VBO);
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices),vertices, GL_STATIC_DRAW);
+    glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE, 3*sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    
     // render loop
     // -----------
     while (!glfwWindowShouldClose(window))
@@ -56,11 +149,19 @@ int main()
         glClearColor(0.4f, 0.3f, 0.3f, 1.0f); // a lovely red wine puke
         glClear(GL_COLOR_BUFFER_BIT);
 
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES,0,3);
+
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
         // -------------------------------------------------------------------------------
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
+
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteProgram(shaderProgram);
 
     // glfw: terminate, clearing all previously allocated GLFW resources.
     // ------------------------------------------------------------------
