5ebfc58a4e6fa790966a581b704641a5cb5acde6
BarelyBear <kangas.otso@gmail.com>
2025-01-13 21:02:48 +0200

    Working file reading for shader setup

--- Getting_Started/Shaders/main.cpp
+++ Getting_Started/Shaders/main.cpp
@@ -1,5 +1,6 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include "shader.h"
 
 #include <iostream>
 
@@ -10,29 +11,6 @@ void processInput(GLFWwindow *window);
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
-const char *vertexShaderSource = "#version 330 core\n"
-    "layout (location = 0) in vec3 aPos;\n"
-    "layout (location = 1) in vec3 aColor;\n"
-    "out vec3 ourColor;\n"
-    "void main()\n"
-    "{\n"
-    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-    "   ourColor = aColor;\n"
-    "}\0"; 
-
-const char *yelloworangeFragmentShaderSource = "#version 330 core\n"
-"out vec4 FragColor;\n"
-"in vec3 ourColor;\n"
-"void main()\n"
-"{\n"
- "   FragColor = vec4(ourColor,1.0);\n"
-"}\0";
-
-struct vec3 {
-    float x;
-    float y;
-    float z;
-};
 
 float rect_vertices[] = {
      0.5f,  0.5f, 0.0f,  // top right
@@ -78,66 +56,13 @@ int main()
         return -1;
     }
 
-    /* Setting up shaders
-    Vertex shader: issue creation from OpenGL, send sourcecode, compile, check for errors. Same algo for fragment.
-    */ 
-
-    unsigned int vertexShader;
-    vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertexShader,1,&vertexShaderSource,NULL);
-    glCompileShader(vertexShader);
-
-    int success;
-    char infoLog[512];
-    glGetShaderiv(vertexShader,GL_COMPILE_STATUS, &success);
-
-    if(!success) {
-        glGetShaderInfoLog(vertexShader,512,NULL,infoLog);
-        std::cout << "Vertex Shader not compiled\n" << infoLog << std::endl;
-    }
-
-    unsigned int yellowFragmentShader;
-    yellowFragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(yellowFragmentShader,1,&yelloworangeFragmentShaderSource,NULL);
-    glCompileShader(yellowFragmentShader);
-
-     glGetShaderiv(yellowFragmentShader,GL_COMPILE_STATUS, &success);
-    if(!success) {
-        glGetShaderInfoLog(yellowFragmentShader,512,NULL,infoLog);
-        std::cout << "Yellow Fragment Shader not compiled\n" << infoLog << std::endl;
-    }
-
-    /*
-    High level shader program : 
-    linked version of executable and shaders. 
-    This program is what we use in the main render loop.
-    */ 
-
-    unsigned int shaderProgram;
-    shaderProgram = glCreateProgram();
-    glAttachShader(shaderProgram,vertexShader);
-    glAttachShader(shaderProgram,yellowFragmentShader);
-
-    
-
-
-    glLinkProgram(shaderProgram);
-
-    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-
-    if(!success) { glGetProgramInfoLog(shaderProgram,512,NULL,infoLog);}
-
-
-    glDeleteShader(vertexShader);
-    glDeleteShader(yellowFragmentShader);
-
+    Shader ourShader("shader.vs","shader.fs");
 
     float vert[] = {
         //vertices       // colors
      0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,  
     -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   
      0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f   
-
     };
 
         /*
@@ -182,12 +107,8 @@ int main()
         glClearColor(0.4f, 0.3f, 0.3f, 1.0f); // a lovely red wine puke
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glUseProgram(shaderProgram);
-
-        float timeValue = glfwGetTime();
-        float greenValue = sin(timeValue) / 2.0f + 0.5f;
-        int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
-        glUniform4f(vertexColorLocation,0.0f,greenValue,0.0f,1.0f);
+        ourShader.use();
+        
         glBindVertexArray(VAO);
         glDrawArrays(GL_TRIANGLES,0,3);
        // glDrawElements(GL_TRIANGLES,6,GL_UNSIGNED_INT, 0);
@@ -202,7 +123,7 @@ int main()
 
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
-    glDeleteProgram(shaderProgram);
+ 
 
     // glfw: terminate, clearing all previously allocated GLFW resources.
     // ------------------------------------------------------------------
