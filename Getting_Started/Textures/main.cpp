7caac20b8292bef6ffc09f82a88d230a096aca34
BarelyBear <kangas.otso@gmail.com>
2025-01-16 18:17:10 +0200

    Tutorial chapter completed with working texture blending.

--- Getting_Started/Textures/main.cpp
+++ Getting_Started/Textures/main.cpp
@@ -47,9 +47,9 @@ int main()
     Shader ourShader("shader.vs","shader.fs");
 
 
-    unsigned int texture;
-    glGenTextures(1, &texture);
-    glBindTexture(GL_TEXTURE_2D, texture);
+    unsigned int texture1,texture2;
+    glGenTextures(1, &texture1);
+    glBindTexture(GL_TEXTURE_2D, texture1);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
@@ -63,9 +63,29 @@ int main()
         glGenerateMipmap(GL_TEXTURE_2D);
     }
     else {
-        std::cout << "Failed to load texture" << std::endl;
+        std::cout << "Failed to load texture"<< texture1 << std::endl;
     }
     stbi_image_free(data);
+
+    
+    glGenTextures(1, &texture2);
+    glBindTexture(GL_TEXTURE_2D, texture2);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_LINEAR);
+
+    stbi_set_flip_vertically_on_load(true);
+    data = stbi_load("../../resources/face.png", &width, &heigth, &nrChannels, 0);
+    if(data){
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width,heigth,0,GL_RGBA,GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else {
+        std::cout << "Failed to load texture" << texture2 << std::endl;
+    }
+    stbi_image_free(data);
+
 
 float vertices[] = {
     // positions          // colors           // texture coords
@@ -119,19 +139,28 @@ unsigned int indices[] = {  // note that we start from 0!
     glVertexAttribPointer(2,2,GL_FLOAT,GL_FALSE, 8 *sizeof(float), (void*)(6*sizeof(float)));
     glEnableVertexAttribArray(2);
 
-    glBindTexture(GL_TEXTURE_2D,texture);
+  
+
+    
     
     while (!glfwWindowShouldClose(window))
     {
        
         processInput(window);
 
-        // render
-        // ------
+   
         glClearColor(0.4f, 0.3f, 0.3f, 1.0f); // a lovely red wine puke
         glClear(GL_COLOR_BUFFER_BIT);
 
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D,texture1);
+        glActiveTexture(GL_TEXTURE1);
+        glBindTexture(GL_TEXTURE_2D,texture2);
+        glBindVertexArray(VAO);
+
         ourShader.use();
+        ourShader.setInt("texture1",0);
+        ourShader.setInt("texture2",1);
         
        // glBindVertexArray(VAO);
         //glDrawArrays(GL_TRIANGLES,0,3);
