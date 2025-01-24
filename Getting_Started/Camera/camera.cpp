7831a863cc94d02e3fe294a531c7cbdd40d56f5e
BarelyBear <kangas.otso@gmail.com>
2025-01-24 14:35:19 +0200

    Unable to finish exercise 2. Work continues next week.

--- Getting_Started/Camera/camera.cpp
+++ Getting_Started/Camera/camera.cpp
@@ -17,8 +17,36 @@ Camera::Camera(glm::vec3 position, glm::vec3 up, float yaw, float pitch)
 
 }
 
-glm::mat4 Camera::GetViewMatrix(){
-  return glm::lookAt(Position, Position + Front, Up);
+glm::mat4 Camera::GetViewMatrix(){  
+
+
+/*
+24.1.
+DISCLOSURE : Here was the original implementation. When comparing to source code and trying to
+run the program, the camera is obviously not working as intended.
+
+There's some dissecting work to be done here next time I sit down to study. I will seek to improve my solution
+iteratively be rereading the chapter start, and only after some time refer to source code.
+
+  glm::vec4 R = glm::vec4(Right,0.0);
+  glm::vec4 U = glm::vec4(Up,0.0);
+  glm::vec4 D = glm::vec4(Front, 0.0);
+
+  glm::mat4x4 rotation =  {R.x, U.x, D.x, 0.0,
+                          R.y, U.y, D.y, 0.0,
+                          R.z, U.z, D.z, 0.0,
+                          0.0, 0.0, 0.0, 1.0};
+  
+  glm::mat4x4 translation = {1.0,0.0,0.0, -Position.x,
+                        0.0,1.0,0.0, -Position.y,
+                        0.0,0.0,1.0, -Position.z,
+                        0.0,0.0,0.0,1.0};
+  
+  return rotation * translation;
+
+  */
+
+
 }
 
 void Camera::ProcessKeyboard(Camera_Movement direction, float deltaTime){
@@ -35,7 +63,7 @@ void Camera::ProcessKeyboard(Camera_Movement direction, float deltaTime){
   if(direction == RIGHT){
     Position += Right * velocity;
   }
-  // This bit was stolen from the exercise solution.
+  // This bit was stolen from the exercise solution. It did not fix my weird bug.
   Position.y = 0.0f;
 }
 
