8e80874a9e1e3fe4d50d2bcc7fb2da7e0dd2fb1c
BarelyBear <kangas.otso@gmail.com>
2025-02-02 16:35:30 +0200

    Finished tutorial chapter. Next exercises.

--- Lighting/Materials/main.cpp
+++ Lighting/Materials/main.cpp
@@ -163,8 +163,16 @@ int main()
 
         lightingShader.use();
         lightingShader.setVec3("viewPos", cameroni.Position);
-        lightingShader.setVec3("light.ambient",  0.2f, 0.2f, 0.2f);
-        lightingShader.setVec3("light.diffuse",  0.5f, 0.5f, 0.5f); // darken diffuse light a bit
+        glm::vec3 lightColor;
+        lightColor.x = sin(glfwGetTime() * 2.0f);
+        lightColor.y = sin(glfwGetTime() * 0.7f);
+        lightColor.z = sin(glfwGetTime() * 1.3f);
+
+        glm::vec3 diffuseColor = lightColor   * glm::vec3(0.5f); 
+        glm::vec3 ambientColor = diffuseColor * glm::vec3(0.2f);    
+
+        lightingShader.setVec3("light.ambient", ambientColor);
+        lightingShader.setVec3("light.diffuse",  diffuseColor); 
         lightingShader.setVec3("light.specular", 1.0f, 1.0f, 1.0f);
         lightingShader.setVec3("light.position", lightPos);
 
--- Lighting/Materials/shaders/lightsource.vs
+++ Lighting/Materials/shaders/lightsource.vs
@@ -1,6 +1,8 @@
 #version 330 core
 layout (location = 0) in vec3 aPos;
 
+in vec3 LightColor;
+
 uniform mat4 model;
 uniform mat4 view;
 uniform mat4 projection;
