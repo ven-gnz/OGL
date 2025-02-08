4eed1aff5ef5c1648111e97599e9903a37c72647
BarelyBear <kangas.otso@gmail.com>
2025-02-08 14:12:36 +0200

    Missing semicolon found. Uttorial chapter finished.

--- Lighting/Multiple_lights/shaders/shader.fs
+++ Lighting/Multiple_lights/shaders/shader.fs
@@ -3,7 +3,7 @@ out vec4 FragColor;
 
 struct Material {
     sampler2D diffuse;
-    sampler2D specular;    
+    sampler2D specular;
     float shininess;
 };
 
@@ -16,14 +16,16 @@ struct DirLight {
 
 struct PointLight {
     vec3 position;
+
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
     
     float constant;
     float linear;
     float quadratic;
 
-    vec3 ambient;
-    vec3 diffuse;
-    vec3 specular;
+
 };
 
 struct SpotLight {
@@ -56,8 +58,8 @@ uniform PointLight pointLights[NR_POINT_LIGHTS];
 uniform SpotLight spotLight;
 
 vec3 CalcDirLight(DirLight light, vec3 normal, vec3 viewDir);
-vec3 CalcPointLight(PointLight light, vec3 normal, vec3 FragPos, vec3 viewDir);
-vec3 CalcSpotLight(SpotLight light, vec3 normal, vec3 FragPos, vec3 viewDir);
+vec3 CalcPointLight(PointLight light, vec3 normal, vec3 fragPos, vec3 viewDir);
+vec3 CalcSpotLight(SpotLight light, vec3 normal, vec3 fragPos, vec3 viewDir);
 
 
 
@@ -69,10 +71,10 @@ void main()
     vec3 viewDir = normalize(viewPos - FragPos);
 
     vec3 result = CalcDirLight(dirLight, norm, viewDir);
-    for(int i = 0; i < NR_POINT_LIGHTS; i++){
-        result += CalcPointLight(PointLight[i], norm, FragPos, viewDir);
-    }
+    for(int i = 0; i < NR_POINT_LIGHTS; i++)
+        result += CalcPointLight(pointLights[i], norm, FragPos, viewDir);
     
+    result += CalcSpotLight(spotLight, norm, FragPos, viewDir);
     FragColor = vec4(result, 1.0);
     
     
@@ -87,38 +89,38 @@ vec3 CalcDirLight(DirLight light, vec3 normal, vec3 viewDir)
     vec3 reflectDir = reflect(-lightDir, normal);  
     float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
     
-    vec3 ambient = light.ambient * vec3(texture(material.diffuse, TexCoords).rgb);
-    vec3 diffuse = light.diffuse * diff * vec3(texture(material.diffuse, TexCoords).rgb);  
-    vec3 specular = light.specular * spec * vec3(texture(material.specular, TexCoords).rgb);
+    vec3 ambient = light.ambient * vec3(texture(material.diffuse, TexCoords));
+    vec3 diffuse = light.diffuse * diff * vec3(texture(material.diffuse, TexCoords));  
+    vec3 specular = light.specular * spec * vec3(texture(material.specular, TexCoords));
 
     return (ambient+diffuse+specular);
 }
 
-vec3 CalcPointLight(PointLight light, vec3 normal, vec3 FragPos, vec3 viewDir)
+vec3 CalcPointLight(PointLight light, vec3 normal, vec3 fragPos, vec3 viewDir)
 {
 
-    vec3 lightDir = normalize(light.position - FragPos);
+    vec3 lightDir = normalize(light.position - fragPos);
 
     float diff = max(dot(normal, lightDir), 0.0);
 
-    vec3 reflectDir = reflect(-lightDir,normal);
+    vec3 reflectDir = reflect(-lightDir, normal);
     float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
 
     // attenuation
-    float distance    = length(light.position - FragPos);
+    float distance    = length(light.position - fragPos);
     float attenuation = 1.0 / (light.constant + light.linear * distance + light.quadratic * (distance * distance));
 
-    vec3 ambient = light.ambient * vec3(texture(material.diffuse, TexCoords).rgb);
-    vec3 diffuse = light.diffuse * diff * vec3(texture(material.diffuse, TexCoords).rgb);  
-    vec3 specular = light.specular * spec * vec3(texture(material.specular, TexCoords).rgb);
+    vec3 ambient = light.ambient * vec3(texture(material.diffuse, TexCoords));
+    vec3 diffuse = light.diffuse * diff * vec3(texture(material.diffuse, TexCoords));  
+    vec3 specular = light.specular * spec * vec3(texture(material.specular, TexCoords));
 
     ambient  *= attenuation;
     diffuse  *= attenuation;
-    specular *= attenuation
+    specular *= attenuation;
     return (ambient + diffuse + specular);
 }
 
-vec3 CalcSpotLight(SpotLight light, vec3 normal, vec3 FragPos, vec3 viewDir)
+vec3 CalcSpotLight(SpotLight light, vec3 normal, vec3 fragPos, vec3 viewDir)
 {
 
     vec3 lightDir = normalize(light.position - fragPos);
